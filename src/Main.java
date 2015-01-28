81c67ea5cd29c54f439d95694cca9a454092e382
othman853 <anuar.yasser@gmail.com>
2015-01-28 16:09:48 -0200

    <Alexandre e Ana> Exercicio de Codificação B - Parte 3 - Versão Melhorada.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
 
 	private static int valor = 0;
-	
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
@@ -17,31 +17,36 @@ public class Main {
 			try {
 				System.out.println("\nDigite o valor do saque");
 				valor = input.nextInt();
-				cont100 = contaValor(100);
-				cont50 = contaValor(50);
-				cont20 = contaValor(20);
-				cont10 = contaValor(10);
-				
-				
-				if (valor < 10 && valor > 0) {
-					System.out.println("Não temos notas menores que 10!");
-				}
+				if (valor < 10) {
+					System.out
+							.println("Por favor bote um valor maior que R$ 10");
+				} else {
+					cont100 = contaValor(100);
+					cont50 = contaValor(50);
+					cont20 = contaValor(20);
+					cont10 = contaValor(10);
 
-				System.out.print("Resultado esperado: ");
+					if (valor < 10 && valor > 0) {
+						System.out
+								.println("Não temos notas disponíveis para R$ "
+										+ valor);
+					}
 
-				for (int i = 0; i < cont100; i++) {
-					System.out.print("R$100,00 ");
-				}
-				for (int i = 0; i < cont50; i++) {
-					System.out.print("R$50,00 ");
-				}
-				for (int i = 0; i < cont20; i++) {
-					System.out.print("R$20,00 ");
-				}
-				for (int i = 0; i < cont10; i++) {
-					System.out.print("R$10,00 ");
-				}
+					System.out.print("Resultado esperado: ");
 
+					for (int i = 0; i < cont100; i++) {
+						System.out.print("R$100,00 ");
+					}
+					for (int i = 0; i < cont50; i++) {
+						System.out.print("R$50,00 ");
+					}
+					for (int i = 0; i < cont20; i++) {
+						System.out.print("R$20,00 ");
+					}
+					for (int i = 0; i < cont10; i++) {
+						System.out.print("R$10,00 ");
+					}
+				}
 			} catch (InputMismatchException e) {
 				System.out.println("Por favor, apenas números inteiros!");
 				valor = 0;
@@ -50,8 +55,8 @@ public class Main {
 		}
 		input.close();
 	}
-	
-	public static int contaValor(int nota){
+
+	public static int contaValor(int nota) {
 		int contador = 0;
 		while (valor >= nota) {
 			valor -= nota;
