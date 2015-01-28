cfb0561c103f6ab0bab3015e6b9a77bd49f9b9f4
othman853 <anuar.yasser@gmail.com>
2015-01-28 16:14:10 -0200

    <Alexandre e Ana> Exercicio de Codificação B - Parte 4 - Refatoração Master.

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,6 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		int cont100;
-		int cont50;
-		int cont20;
-		int cont10;
-
 		while (valor > -1) {
 			try {
 				System.out.println("\nDigite o valor do saque");
@@ -21,30 +16,17 @@ public class Main {
 					System.out
 							.println("Por favor bote um valor maior que R$ 10");
 				} else {
-					cont100 = contaValor(100);
-					cont50 = contaValor(50);
-					cont20 = contaValor(20);
-					cont10 = contaValor(10);
+
+					System.out.print("Resultado esperado: ");
+					contaValor(100);
+					contaValor(50);
+					contaValor(20);
+					contaValor(10);
 
 					if (valor < 10 && valor > 0) {
 						System.out
 								.println("Não temos notas disponíveis para R$ "
 										+ valor);
-					}
-
-					System.out.print("Resultado esperado: ");
-
-					for (int i = 0; i < cont100; i++) {
-						System.out.print("R$100,00 ");
-					}
-					for (int i = 0; i < cont50; i++) {
-						System.out.print("R$50,00 ");
-					}
-					for (int i = 0; i < cont20; i++) {
-						System.out.print("R$20,00 ");
-					}
-					for (int i = 0; i < cont10; i++) {
-						System.out.print("R$10,00 ");
 					}
 				}
 			} catch (InputMismatchException e) {
@@ -56,12 +38,10 @@ public class Main {
 		input.close();
 	}
 
-	public static int contaValor(int nota) {
-		int contador = 0;
+	public static void contaValor(int nota) {
 		while (valor >= nota) {
 			valor -= nota;
-			contador++;
+			System.out.print("R$ " + nota + ".00, ");
 		}
-		return contador;
 	}
 }
